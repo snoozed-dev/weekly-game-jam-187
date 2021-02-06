5a8387f3f8cfae895403ee562dc9964a30073aaf
Nico <nicolasamaya93@gmail.com>
2021-02-06 17:54:46 -0300

    bl

---
+++ Assets/Scripts/NicoPlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NicoPlayerController : MonoBehaviour
+{
+    public float forwardInput;
+    public float horizontalInput;
+    public float speed = 15.0f;
+    public float turnSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput * speed);
+        transform.Rotate(Vector3.up, Time.deltaTime * horizontalInput * turnSpeed);
+        
+
+    }
+}
